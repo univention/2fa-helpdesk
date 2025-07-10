27fc6098901a20f4f25b0dfde5b1e592737851d0
Jan Burgmeier <jan.burgmeier.extern@univention.de>
2025-07-10 10:27:30 +0200

    fix(frontend): Add unit test for token refresh
    univention/dev/internal/team-nubus#1241

--- frontend/tests/auth.test.ts
+++ frontend/tests/auth.test.ts
@@ -6,6 +6,9 @@
 import { afterEach, beforeEach, describe, expect, it, vi } from "vitest";
 import * as keycloakService from '@/services/keycloak'
 import { logoutUser } from "@/utils/auth";
+import { fetchWhoAmI } from "@/services/whoami.ts"
+import axiosInstance from "@/services/axios";
+import { AxiosHeaders } from "axios";
 
 describe('logoutUser', () => {
   let logoutMock: any;
@@ -37,5 +40,48 @@ describe('logoutUser', () => {
     logoutUser()
 
     expect(logoutMock).toHaveBeenCalledWith({})
+  })
+})
+
+describe('refreshToken', () => {
+  let refreshTokenMock: any;
+  let getTokenMock: any;
+  let axiosGetMock: any;
+  let token = Math.random().toString(36).slice(2);
+
+
+  beforeEach(() => {
+    refreshTokenMock = vi.spyOn(keycloakService, 'refreshToken').mockImplementation(() => { token = Math.random().toString(36).slice(2); })
+    getTokenMock = vi.spyOn(keycloakService, 'getToken').mockImplementation(() => { return token })
+    axiosGetMock = vi.spyOn(axiosInstance, 'get').mockImplementation(() => {
+      axiosInstance.interceptors.request.forEach((interceptor) => {
+        let config = {}
+        config.headers = new AxiosHeaders()
+        interceptor.fulfilled(config)
+      })
+      return Promise.resolve({ data: { success: true, twofa_admin: false } })
+    })
+    vi.stubGlobal('window', {
+      __APP_CONFIG__: {}
+    });
+  });
+
+  afterEach(() => {
+    refreshTokenMock.mockRestore();
+    getTokenMock.mockRestore();
+    axiosGetMock.mockRestore();
+    vi.unstubAllGlobals();
+  })
+
+  it('make axios request', () => {
+    let initToken = token
+    return fetchWhoAmI().then((data) => {
+      expect(axiosGetMock).toHaveBeenCalledTimes(1)
+      expect(getTokenMock).toHaveBeenCalledTimes(1)
+      expect(refreshTokenMock).toHaveBeenCalledTimes(1)
+      expect(initToken).not.toBe(token)
+      expect(data.success).toBe(true)
+      expect(data.twofa_admin).toBe(false)
+    })
   })
 })
